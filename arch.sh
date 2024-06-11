9ddbbf807d2b233c2b6214e344c8138d91605be5
Michał Panowicz <panowicz.michal@gmail.com>
2024-06-11 20:46:05 +0200

    Update to 2.0.0-1

--- arch.sh
+++ arch.sh
@@ -5,11 +5,10 @@ if [ "$TARGETARCH" = "arm" ] ; then ARCH=arm;fi
 
 VER=$1
 BUILD_VER=$2
-ZIP_URL = "https://github.com/h9-dev/chia-miner/releases/download/v${VER}/HPool-Miner-chia-og-v${VER}-${BUILD_VER}-linux.zip "
-
+ZIP_URL="https://github.com/h9-dev/chia-miner/releases/download/v${VER}/H9-Miner-chia-og-v${VER}-${BUILD_VER}-linux.zip"
 
 echo ${ZIP_URL}
 wget -q --no-check-certificate ${ZIP_URL} -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip -d /tmp/linux
 ls /tmp/linux -ahl
-mv /tmp/linux/hpool-miner-chia-linux-${ARCH} /opt/hpool-chia-miner
-chmod +x /opt/hpool-chia-miner
+mv /tmp/linux/h9-miner-chia-linux-${ARCH} /opt/h9-chia-miner
+chmod +x /opt/h9-chia-miner
